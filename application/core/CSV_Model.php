365b956e503769238ad2e56f35d3fff2b539d3cf
Master Yoda <jim_parry@bcit.ca>
2018-02-08 22:08:16 -0800

    Missed entity usage in CSV_Model - fixed Signed-off-by:Master Yoda <jim_parry@bcit.ca>

--- application/core/CSV_Model.php
+++ application/core/CSV_Model.php
@@ -61,7 +61,7 @@ class CSV_Model extends Memory_Model
 				else
 				{
 					// build object from a row
-					$record = new $this->entity();
+					$record = new stdClass();
 					for ($i = 0; $i < count($this->_fields); $i ++ )
 						$record->{$this->_fields[$i]} = $data[$i];
 					$key = $record->{$this->_keyfield};
